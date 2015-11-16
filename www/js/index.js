f15d7bba706e2f5bcd61c28c96ce686818584083
João Williams Balaniúc <jwillbala@hotmail.com>
2015-11-16 17:41:17 -0200

    BETA-FINAL

--- www/js/index.js
+++ www/js/index.js
@@ -36,6 +36,7 @@ var app = {
         
         profileCreate();
         statusCreate();
+        locCreate();
         
         ping();
         
--- www/js/ping.js
+++ www/js/ping.js
@@ -2,6 +2,7 @@
 // PING RADIUS
 //=============================
 function ping() {
+    sessionStorage.status = 0; // offline
     if (!localStorage.cliNome) {
         return false;
     }
@@ -10,6 +11,7 @@ function ping() {
     req.onreadystatechange = function () {
         if (req.readyState == 4) {
             if (req.status == 200) {
+                sessionStorage.status = 1; // online
                 var data = req.responseText;
                 var res = data.split(";");
                 console.log(data);
@@ -20,6 +22,18 @@ function ping() {
                 localStorage.statusBrowser = res[4];
                 localStorage.statusDate = res[5];
                 statusCreate();
+                //
+                localStorage.locImg = res[7];
+                localStorage.locTitle = res[8];
+                localStorage.locTxt = res[9];
+                localStorage.locEmail = res[10];
+                localStorage.locSubject = res[11];
+                localStorage.locPhone = res[12];
+                localStorage.locFace = res[13];
+                localStorage.locGoogle = res[14];
+                localStorage.locTwitter = res[15];
+                locCreate();
+                console.log(localStorage);
             }
         }
     };
@@ -83,5 +97,72 @@ function profileCreate() {
         $('#LoginForm').show();
         $("#LoginOK").hide();
         $("#LoginDO").show();
+    }
+}
+
+//=============================
+// POPULAR INFO DO LOCAL
+//=============================
+function locCreate() {
+    if (localStorage.status) {
+
+        $('#goMail').val("Enviar para " + localStorage.statusLoc);
+        $('#locPhone').attr("href", "tel:" + localStorage.locPhone);
+
+        $('.cliLoc').html(localStorage.statusLoc);
+
+        $('#cliContato').val(localStorage.cliNome + " <" + localStorage.cliEmail + ">");
+
+        if (localStorage.locTitle != "")
+            $('#locTitle').html(localStorage.locTitle);
+        else
+            $('#locTitle').html("Bem-vindo a SempreON");
+
+        if (localStorage.locTxt != "")
+            $('#locTxt').html(localStorage.locTxt);
+        else
+            $('#locTxt').html("O sinal de wifi grátis que você utilizou é uma cortesia de <strong>" + localStorage.cliLoc + "</strong>. Este serviço não é um direito, e sim um privilégio! Use com responsabilidade, e aproveite!");
+
+        if (!localStorage.locImg || sessionStorage.status == 0) {
+            $('#locImg').attr("src", "images/icons/pointer.png");
+        }
+        else {
+
+            $('#locImg').attr("src", "http://sempreon.mobi/hotspot/upload/" + localStorage.locImg);
+        }
+        if (localStorage.locEmail != "") {
+            $('#faleconosco').show();
+            $('#locEmail').val(localStorage.locEmail);
+            $('#locSubject').val(localStorage.locSubject);
+        }
+        else {
+            $('#faleconosco').hide();
+        }
+        if (localStorage.locFacebook || localStorage.locTwitter || localStorage.locGoogle) {
+            $('#siganos').show();
+
+            if (localStorage.locFace != "")
+                $('#locFace').show();
+            else
+                $('#locFace').hide();
+
+            if (localStorage.locTwitter != "")
+                $('#locTwitter').show();
+            else
+                $('#locTwitter').hide();
+
+            if (localStorage.locGoogle != "")
+                $('#locGoogle').show();
+            else
+                $('#locGoogle').hide();
+        }
+        else {
+            $('#siganos').hide();
+        }
+
+        if (localStorage.locPhone != "")
+            $('#liguenos').show();
+        else
+            $('#liguenos').hide();
     }
 }
